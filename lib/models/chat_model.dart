67e9c0d1cfa500f54afe0b48b21f755e373e0e87
REYHAB <rahabwanjiku96@gmail.com>
2019-03-01 12:26:15 +0300

    Whatsapp ui clone

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,44 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message, this.time, this.avatarUrl});
+
+}
+  List<ChatModel> dummyData = [
+    new ChatModel(
+      name: "RAHAB ",
+      message: " Reyhab is loving flutter!",
+      time: "15.57",
+      avatarUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F902761456579608577%2F0aFE33qR_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fprettiest_giris&docid=9TcnkJ7NMXN8GM&tbnid=TcoT0WSNg-24eM%3A&vet=10ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ..i&w=400&h=400&bih=657&biw=1366&q=pictures%20of%20beautiful%20girls&ved=0ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ&iact=mrc&uact=8"),
+
+      new ChatModel(
+      name: "KENNEDY ",
+      message: "He is my best_friend",
+      time: "15.57",
+      avatarUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F902761456579608577%2F0aFE33qR_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fprettiest_giris&docid=9TcnkJ7NMXN8GM&tbnid=TcoT0WSNg-24eM%3A&vet=10ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ..i&w=400&h=400&bih=657&biw=1366&q=pictures%20of%20beautiful%20girls&ved=0ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ&iact=mrc&uact=8"),
+
+      new ChatModel(
+      name: "JOHN ",
+      message: "Am a developer and I love it",
+      time: "01.00",
+      avatarUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F902761456579608577%2F0aFE33qR_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fprettiest_giris&docid=9TcnkJ7NMXN8GM&tbnid=TcoT0WSNg-24eM%3A&vet=10ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ..i&w=400&h=400&bih=657&biw=1366&q=pictures%20of%20beautiful%20girls&ved=0ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ&iact=mrc&uact=8"),
+
+      new ChatModel(
+      name: "KIKI ",
+      message: "Kiki was my only female classmate",
+      time: "2.00",
+      avatarUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F902761456579608577%2F0aFE33qR_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fprettiest_giris&docid=9TcnkJ7NMXN8GM&tbnid=TcoT0WSNg-24eM%3A&vet=10ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ..i&w=400&h=400&bih=657&biw=1366&q=pictures%20of%20beautiful%20girls&ved=0ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ&iact=mrc&uact=8"),
+
+      new ChatModel(
+      name: "ERICK ",
+      message: "Hey there Erik you are an amazing brother ",
+      time: "16.57",
+      avatarUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F902761456579608577%2F0aFE33qR_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fprettiest_giris&docid=9TcnkJ7NMXN8GM&tbnid=TcoT0WSNg-24eM%3A&vet=10ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ..i&w=400&h=400&bih=657&biw=1366&q=pictures%20of%20beautiful%20girls&ved=0ahUKEwig6IOR-9vgAhWKzoUKHXk5DDcQMwheKAEwAQ&iact=mrc&uact=8"),
+    
+  ];
+
+  
+  
---
+++ lib/pages/camera_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return new Center(
+      child: new Text("Camera",
+      style: new TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
